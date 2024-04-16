1358e0b391577990cb572f4cfb1314f578c75188
adrivargas <56644122+adrivargas@users.noreply.github.com>
2024-04-16 10:43:49 -0500

    Conexion a la base de datos

--- connect.js
+++ connect.js
@@ -1,10 +1,27 @@
+const { MongoClient } = require('mongodb');
 const config = require('./config');
 
-// eslint-disable-next-line no-unused-vars
-const { dbUrl } = config;
-
 async function connect() {
-  // TODO: Database Connection
+  const { dbUrl } = config;
+
+  try {
+    // Crear un nuevo cliente de MongoDB
+    const client = new MongoClient(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    // Conectar al cliente
+    await client.connect();
+
+    console.log('Conexión a la base de datos establecida.');
+
+    // Devolver el objeto de la base de datos
+    return client.db("prueba");
+  } catch (error) {
+    console.error('Error al conectar a la base de datos:', error.message);
+    throw error; // Re-lanzar el error para que sea manejado por el código que llama a esta función
+  }
 }
 
 module.exports = { connect };
+
+
+
